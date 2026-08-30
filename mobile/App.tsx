4b3a949447e81359fd1a98c15ac50288eb4e37ac
agustinperalta745-ai <agustinperalta745@gmail.com>
2026-08-30 04:07:16 -0300

    Use AJPA menu background app shell

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -6,7 +6,7 @@ import {
   initialWindowMetrics,
 } from 'react-native-safe-area-context';
 
-import LiveApp from './src/LiveApp';
+import LiveAppBackgrounds from './src/LiveAppBackgrounds';
 
 export default function App() {
   return (
@@ -20,7 +20,7 @@ export default function App() {
           backgroundColor="#02060a"
           translucent={false}
         />
-        <LiveApp />
+        <LiveAppBackgrounds />
       </SafeAreaView>
     </SafeAreaProvider>
   );
