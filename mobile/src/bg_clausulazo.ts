e16ce61bf8c162a0993203dc2ddbd246caaa29e5
agustinperalta745-ai <agustinperalta745@gmail.com>
2026-08-31 00:57:59 -0300

    Add dedicated Clausulazo Figo background

---
+++ mobile/src/bg_clausulazo.ts
@@ -0,0 +1,15 @@
+import { PART_0 } from './bg_clausulazo_parts/part0';
+import { PART_1 } from './bg_clausulazo_parts/part1';
+import { PART_2 } from './bg_clausulazo_parts/part2';
+import { PART_3 } from './bg_clausulazo_parts/part3';
+import { PART_4 } from './bg_clausulazo_parts/part4';
+import { PART_5 } from './bg_clausulazo_parts/part5';
+
+export const BG_CLAUSULAZO =
+  'data:image/jpeg;base64,' +
+  PART_0 +
+  PART_1 +
+  PART_2 +
+  PART_3 +
+  PART_4 +
+  PART_5;
