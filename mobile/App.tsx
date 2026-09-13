d7cf8d909a6c35ef82a8f141110d7d0eb257fbe5
agustinperalta745-ai <agustinperalta745@gmail.com>
2026-09-13 00:05:35 -0300

    refactor(mobile): remove floating trophy cabinet

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -10,7 +10,6 @@ import {
 import MatchSearchShell from './src/MatchSearchShell';
 import CompetitionCycleAdminFab from './src/CompetitionCycleAdminFab';
 import SeasonHistoryFab from './src/SeasonHistoryFab';
-import TrophyCabinetFab from './src/TrophyCabinetFab';
 import SeasonCountdownBanner from './src/SeasonCountdownBanner';
 import CupCenterFab from './src/CupCenterFab';
 
@@ -72,7 +71,6 @@ export default function App() {
         <MatchSearchShell />
         <CupCenterFab />
         <SeasonHistoryFab />
-        <TrophyCabinetFab />
         <CompetitionCycleAdminFab />
       </SafeAreaView>
     </SafeAreaProvider>
