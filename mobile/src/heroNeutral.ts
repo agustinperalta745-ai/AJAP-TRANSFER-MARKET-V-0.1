0ad8fd0b6267ce4ad5f88b6062fd5626f5a12e32
agustinperalta745-ai <agustinperalta745@gmail.com>
2026-09-25 05:06:00 -0300

    Assemble neutral AJPA hero background

---
+++ mobile/src/heroNeutral.ts
@@ -0,0 +1,9 @@
+import p1 from './heroNeutralPart1';
+import p2 from './heroNeutralPart2';
+import p3 from './heroNeutralPart3';
+import p4 from './heroNeutralPart4';
+import p5 from './heroNeutralPart5';
+import p6 from './heroNeutralPart6';
+
+export const HERO_NEUTRAL = `data:image/jpeg;base64,${p1}${p2}${p3}${p4}${p5}${p6}`;
+export default HERO_NEUTRAL;
